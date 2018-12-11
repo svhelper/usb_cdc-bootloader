7be0a333a2c6b5a89c2c1b431ad537b872857240
lupyuen <luppy@appkaki.com>
2018-12-12 02:41:41 +0800

    Testing msc

--- src/usb_conf.c
+++ src/usb_conf.c
@@ -46,86 +46,8 @@ void usb_set_serial_number(const char* serial) {
     }
 }
 
-#define NEW_USB
-#ifdef NEW_USB
-//  From https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/f4/stm32f4-discovery/usb_msc/msc.c
-
-static const struct usb_device_descriptor dev_descr = {
-	.bLength = USB_DT_DEVICE_SIZE,
-	.bDescriptorType = USB_DT_DEVICE,
-	.bcdUSB = 0x0110,
-	.bDeviceClass = 0,
-	.bDeviceSubClass = 0,
-	.bDeviceProtocol = 0,
-	.bMaxPacketSize0 = 64,
-	.idVendor = 0x0483,
-	.idProduct = 0x5741,
-	.bcdDevice = 0x0200,
-	.iManufacturer = 1,
-	.iProduct = 2,
-	.iSerialNumber = 3,
-	.bNumConfigurations = 1,
-};
-
-static const struct usb_endpoint_descriptor msc_endp[] = {{
-	.bLength = USB_DT_ENDPOINT_SIZE,
-	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x01,
-	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
-	.wMaxPacketSize = 64,
-	.bInterval = 0,
-}, {
-	.bLength = USB_DT_ENDPOINT_SIZE,
-	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x82,
-	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
-	.wMaxPacketSize = 64,
-	.bInterval = 0,
-}};
-
-static const struct usb_interface_descriptor msc_iface[] = {{
-	.bLength = USB_DT_INTERFACE_SIZE,
-	.bDescriptorType = USB_DT_INTERFACE,
-	.bInterfaceNumber = 0,
-	.bAlternateSetting = 0,
-	.bNumEndpoints = 2,
-	.bInterfaceClass = USB_CLASS_MSC,
-	.bInterfaceSubClass = USB_MSC_SUBCLASS_SCSI,
-	.bInterfaceProtocol = USB_MSC_PROTOCOL_BBB,
-	.iInterface = 0,
-	.endpoint = msc_endp,
-	.extra = NULL,
-	.extralen = 0
-}};
-
-static const struct usb_interface ifaces[] = {{
-	.num_altsetting = 1,
-	.altsetting = msc_iface,
-}};
-
-static const struct usb_config_descriptor config_descr = {
-	.bLength = USB_DT_CONFIGURATION_SIZE,
-	.bDescriptorType = USB_DT_CONFIGURATION,
-	.wTotalLength = 0,
-	.bNumInterfaces = 1,
-	.bConfigurationValue = 1,
-	.iConfiguration = 0,
-	.bmAttributes = 0x80,
-	.bMaxPower = 0x32,
-	.interface = ifaces,
-};
-
-static const char *usb_strings[] = {
-	"Black Sphere Technologies",
-	"MSC Demo",
-	"DEMO",
-};
-
-static usbd_device *msc_dev;
-/* Buffer to be used for control requests. */
-static uint8_t usbd_control_buffer[USB_CONTROL_BUF_SIZE];
-// static uint8_t usbd_control_buffer[128];
-
+#define RAM_DISK
+#ifdef RAM_DISK
 #define WBVAL(x) ((x) & 0xFF), (((x) >> 8) & 0xFF)
 #define QBVAL(x) ((x) & 0xFF), (((x) >> 8) & 0xFF),\
 		 (((x) >> 16) & 0xFF), (((x) >> 24) & 0xFF)
@@ -249,7 +171,7 @@ int ramdisk_init(void)
 
 int ramdisk_read(uint32_t lba, uint8_t *copy_to)
 {
-    // debug_print("ramdisk_read lba "); debug_print_int(lba); debug_println(""); // debug_flush(); ////
+    debug_print("ramdisk_read lba "); debug_print_int(lba); debug_println(""); // debug_flush(); ////
 	memset(copy_to, 0, SECTOR_SIZE);
 	switch (lba) {
 		case 0: // sector 0 is the boot sector
@@ -276,7 +198,7 @@ int ramdisk_read(uint32_t lba, uint8_t *copy_to)
 
 int ramdisk_write(uint32_t lba, const uint8_t *copy_from)
 {
-    // debug_println("ramdisk_write"); // debug_flush(); ////
+    debug_println("ramdisk_write"); // debug_flush(); ////
 	(void)lba;
 	(void)copy_from;
 	// ignore writes
@@ -287,6 +209,111 @@ int ramdisk_blocks(void)
 {
 	return SECTOR_COUNT;
 }
+#endif  //  RAM_DISK
+
+// #define NEW_USB
+#ifdef NEW_USB
+//  From https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/f4/stm32f4-discovery/usb_msc/msc.c
+
+static const struct usb_device_descriptor dev_descr = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0110,
+	.bDeviceClass = 0,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0483,
+	.idProduct = 0x5741,
+	.bcdDevice = 0x0200,
+	.iManufacturer = 1,
+	.iProduct = 2,
+	.iSerialNumber = 3,
+	.bNumConfigurations = 1,
+};
+
+static const struct usb_endpoint_descriptor msc_endp[] = {{
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x01,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 0,
+}, {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x82,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+	.bInterval = 0,
+}};
+
+static const struct usb_interface_descriptor msc_iface[] = {{
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_MSC,
+	.bInterfaceSubClass = USB_MSC_SUBCLASS_SCSI,
+	.bInterfaceProtocol = USB_MSC_PROTOCOL_BBB,
+	.iInterface = 0,
+	.endpoint = msc_endp,
+	.extra = NULL,
+	.extralen = 0
+}};
+
+static const struct usb_interface ifaces[] = {{
+	.num_altsetting = 1,
+	.altsetting = msc_iface,
+}};
+
+static const struct usb_config_descriptor config_descr = {
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+	.bNumInterfaces = 1,
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0x80,
+	.bMaxPower = 0x32,
+	.interface = ifaces,
+};
+
+static const struct usb_config_descriptor config = {
+    .bLength = USB_DT_CONFIGURATION_SIZE,
+    .bDescriptorType = USB_DT_CONFIGURATION,
+    .wTotalLength = 0,
+    .bNumInterfaces = sizeof(ifaces) / sizeof(struct usb_interface),
+    .bConfigurationValue = 1,
+    .iConfiguration = 0,
+    .bmAttributes = 0xC0,
+    .bMaxPower = 0x32,
+    .interface = ifaces,
+};
+
+static const struct usb_device_capability_descriptor* capabilities[] = {
+	(const struct usb_device_capability_descriptor*) 
+        &webusb_platform_capability_descriptor,
+};
+
+static const struct usb_bos_descriptor bos_descriptor = {
+	.bLength = USB_DT_BOS_SIZE,
+	.bDescriptorType = USB_DT_BOS,
+	.bNumDeviceCaps = sizeof(capabilities) / sizeof(capabilities[0]),
+	.capabilities = capabilities
+};
+
+static const char *usb_strings[] = {
+	"Black Sphere Technologies",
+	"MSC Demo",
+	"DEMO",
+};
+
+static usbd_device *msc_dev;
+/* Buffer to be used for control requests. */
+static uint8_t usbd_control_buffer[USB_CONTROL_BUF_SIZE];
+// static uint8_t usbd_control_buffer[128];
 
 usbd_device* usb_setup(void) {
     int num_strings = sizeof(usb_strings) / sizeof(const char*);
@@ -300,6 +327,13 @@ usbd_device* usb_setup(void) {
 	usb_msc_init(msc_dev, 0x82, 64, 0x01, 64, "VendorID", "ProductID", "0.00", 
         ramdisk_blocks(), ramdisk_read, ramdisk_write);
         // UF2_NUM_BLOCKS, read_block, write_block);
+
+    ////debug_println("bootloader dfu_setup");  debug_flush();
+    ////dfu_setup(usbd_dev, &target_manifest_app, NULL, NULL);
+
+	usb21_setup(msc_dev, &bos_descriptor);
+	webusb_setup(msc_dev, origin_url);
+	winusb_setup(msc_dev, 0);
 
     //  From https://github.com/thirdpin/pastilda/blob/master/emb/pastilda/usb/usb_device/usbd_composite.cpp
 	cm_disable_interrupts();
@@ -433,17 +467,24 @@ usbd_device* usb_setup(void) {
         usb_strings, num_strings,
         usbd_control_buffer, sizeof(usbd_control_buffer));
     
-    debug_println("bootloader usb_msc_init");  debug_flush();
-    usb_msc_init(usbd_dev, 0x82, 64, 0x01, 64, "Example Ltd", "UF2 Bootloader",
-	    "42.00", UF2_NUM_BLOCKS, read_block, write_block);
-    debug_println("bootloader usb_msc_init done");  debug_flush();        
-
-    ////debug_println("bootloader dfu_setup");  debug_flush();
+    usb_msc_init(usbd_dev, 0x82, 64, 0x01, 64, "Example Ltd", "UF2 Bootloader", "42.00", 
+#ifdef RAM_DISK    
+        ramdisk_blocks(), ramdisk_read, ramdisk_write
+#else
+        UF2_NUM_BLOCKS, read_block, write_block
+#endif  //  RAM_DISK        
+    );
     ////dfu_setup(usbd_dev, &target_manifest_app, NULL, NULL);
+	usb21_setup(usbd_dev, &bos_descriptor);
+	webusb_setup(usbd_dev, origin_url);
+	winusb_setup(usbd_dev, 0);
 
-	////usb21_setup(usbd_dev, &bos_descriptor);
-	////webusb_setup(usbd_dev, origin_url);
-	////winusb_setup(usbd_dev, 0);
+    //  From https://github.com/thirdpin/pastilda/blob/master/emb/pastilda/usb/usb_device/usbd_composite.cpp
+	cm_disable_interrupts();
+    nvic_set_priority(NVIC_OTG_FS_IRQ, 0x01<<7);
+	nvic_enable_irq(NVIC_OTG_FS_IRQ);
+    cm_enable_interrupts();
+
     return usbd_dev;
 }
 #endif  //  NEW_USB
